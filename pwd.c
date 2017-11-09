bc29a7bbf6219216edfc78b76c17d6fbece56c24
Antoine Guillory <antoineguillory@hotmail.fr>
2017-11-09 11:57:46 +0100

    Add files via upload

---
+++ pwd.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+#include <pwd.h>
+#include <dirent.h>
+#include <grp.h>
+#include <libgen.h>
+
+
+#define CURRENT_DIR "."
+#define PRED_DIR ".."
+
+typedef int file_descriptor_t;
+
+ino_t find_inode_currdir();
+ino_t find_inode(char* name);
+char * find_next_step(ino_t inode_curr, char* pwd);
+bool is_root(void);
+
+int main(void){
+    char* pwd_str = malloc(2);
+    char* pwd_next = malloc(2);
+    DIR *dir;
+    do{
+        if ((dir = opendir ("..")) == NULL) {
+            perror ("Cannot open ..");
+            exit (EXIT_FAILURE);
+        }
+        pwd_next = find_next_step(find_inode_currdir(), pwd_str);
+        strcat(strcat(pwd_next, "/"), pwd_str);
+        printf("DEBUG : NEXT DIR IS %s\n", pwd_next);
+        printf("DEBUG : PWD STATE : %s\n", pwd_str);
+        
+    }while(strcmp(pwd_str,pwd_next)!=0);
+    closedir(dir);
+    printf("%s\n", pwd_str);
+    
+    return 0;
+}
+
+ino_t find_inode_currdir() {
+    return find_inode(CURRENT_DIR);
+}
+
+ino_t find_inode(char* name) {
+    struct stat fd_stat;
+    
+    file_descriptor_t fd = open(name, O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+    int ret_val = fstat(fd, &fd_stat);
+    if (ret_val < 0) {  
+        perror("fstat");
+        exit(EXIT_FAILURE);
+    }
+    
+    ret_val = close(fd);
+    if (ret_val < 0) {  
+        perror("close");
+        exit(EXIT_FAILURE);
+    }
+    
+    return (ino_t) fd_stat.st_ino;
+}
+
+char * find_next_step(ino_t inode_curr, char* pwd){
+    if(is_root()){
+        return pwd;
+    }
+    DIR *dir;
+    struct dirent *dp;
+    if ((dir = opendir ("..")) == NULL) {
+        perror ("Cannot open ..");
+        exit (EXIT_FAILURE);
+    }
+
+    while ((dp = readdir(dir)) != NULL) {
+        if((ino_t)dp->d_ino==inode_curr){
+            closedir(dir);
+            printf("DEBUG : dp is : %s\n", dp->d_name);
+            return dp->d_name;
+        }
+    }
+    
+    exit(EXIT_FAILURE);
+}
+
+bool is_root(void){
+    if (find_inode_currdir() == find_inode(PRED_DIR)){
+        return true;
+    }
+    return false;
+}
